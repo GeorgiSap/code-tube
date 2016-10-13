8cbb9a6bb80202ceddd9be1470ecf45a5093ea6a
GeorgiSap <georgisapundzhiev@hotmail.com>
2016-10-13 18:34:45 +0300

    Configured Elastic Cloud requests

--- src/main/java/com/codetube/model/search/IndexVideoClipDAO.java
+++ src/main/java/com/codetube/model/search/IndexVideoClipDAO.java
@@ -6,28 +6,33 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.codetube.model.tag.Tag;
 import com.codetube.model.user.User;
 import com.codetube.model.videoclip.VideoClip;
 
 public class IndexVideoClipDAO {
+	private ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+	public String pathComponent = (String) context.getBean("pathComponent");
+	
 	public String index(VideoClip videoClip, User user, Tag tag) {
 		HttpURLConnection connection = null;
 		String jsonString = null;
 		String localURL = "http://localhost:9200/videos/video/" + videoClip.getId();
-		String URL = "http://1ae7caf5273f0f6555d2619c07dfabb4.eu-west-1.aws.found.io:9200/videos/video/" + videoClip.getId();
-		String str ="{\"id\": " + videoClip.getId() + 
+		String indexCloudURL = "http://1ae7caf5273f0f6555d2619c07dfabb4.eu-west-1.aws.found.io:9200/" + pathComponent + "/video/" + videoClip.getId();
+		String indexOutput ="{\"id\": " + videoClip.getId() + 
 				", \"tag\": \"" + tag.getKeyword() + 
 				"\", \"title\": \"" + videoClip.getPerformer() + 
 				"\", \"username\": \"" + user.getUserName() + "\"}";
-		System.out.println(str);
-		
+
 		try {
-			connection = (HttpURLConnection) new URL(URL).openConnection();
+			connection = (HttpURLConnection) new URL(indexCloudURL).openConnection();
 			connection.setRequestMethod("PUT");
 			connection.setRequestProperty("Content-Type", "application/json");
 			connection.setDoOutput(true);
-			byte[] outputInBytes = str.getBytes("UTF-8");
+			byte[] outputInBytes = indexOutput.getBytes("UTF-8");
 			OutputStream os = connection.getOutputStream();
 			os.write(outputInBytes);
 			os.close();
--- src/main/java/com/codetube/model/search/SearchQueryDAO.java
+++ src/main/java/com/codetube/model/search/SearchQueryDAO.java
@@ -6,21 +6,27 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class SearchQueryDAO {
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+public class SearchQueryDAO {
+	private ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+	public String pathComponent = (String) context.getBean("pathComponent");
+	
 	public String search(String searchQuery) {
+		
 		HttpURLConnection connection = null;
 		String jsonString = null;
-		String localURL = "http://localhost:9200/_search";
-		String URL = "http://1ae7caf5273f0f6555d2619c07dfabb4.eu-west-1.aws.found.io:9200/_search";
-		String str = "{\"query\": {\"query_string\": {\"query\": \"" + searchQuery + "\"}}}'";
+		String searchLocalURL = "http://localhost:9200/_search";
+		String searchCloudURL = "http://1ae7caf5273f0f6555d2619c07dfabb4.eu-west-1.aws.found.io:9200/" + pathComponent + "/_search";
+		String searchQueryOutput = "{\"query\": {\"query_string\": {\"query\": \"" + searchQuery + "\"}}}'";
 
 		try {
-			connection = (HttpURLConnection) new URL(URL).openConnection();
+			connection = (HttpURLConnection) new URL(searchCloudURL).openConnection();
 			connection.setRequestMethod("POST");
 			connection.setRequestProperty("Content-Type", "application/json");
 			connection.setDoOutput(true);
-			byte[] outputInBytes = str.getBytes("UTF-8");
+			byte[] outputInBytes = searchQueryOutput.getBytes("UTF-8");
 			OutputStream os = connection.getOutputStream();
 			os.write(outputInBytes);
 			os.close();
